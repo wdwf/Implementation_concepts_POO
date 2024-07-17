a7ab60d7a1e7f72eb7bf5aab026dfe610835c3cd
wdwf <w_dwf@hotmail.com>
2024-07-16 23:25:13 -0300

    :construction: inicio de documentação das rotas da api com swagger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response, Router } from "express";
 import routes from "./Routers";
 import { errorMiddleware } from "./Middlewares/error";
+import SwaggerUi from "swagger-ui-express";
+import swaggerDocs from "../swagger.json";
 
 const PORT = 3333;
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+app.use("/api-docs", SwaggerUi.serve, SwaggerUi.setup(swaggerDocs));
+app.use("/v1", routes);
 app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log(`Running server http://localhost:${PORT}`));
